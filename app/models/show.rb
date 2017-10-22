d35ba3cd4a5c13454c52b4c4039367c5f8ec2cc1
Robert Laws <roblaws@me.com>
2017-10-22 15:05:39 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,8 +1,5 @@
 class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
-
-  def build_network(letters)
-    self.network_id = Network.new(letters)
-  end
+  belongs_to :network
 end
